e074ede841ad52587c68c91f44ab8fa879f7deb5
Julian Bertsch <julian.bertsch42@gmail.com>
2019-02-06 12:47:12 +0100

    change holidays in normal calendar as well

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -66,7 +66,7 @@ class CalendarController extends Controller
         }
 
         $year       = \Carbon\Carbon::now()->year;
-        $holidays   = file_get_contents('http://feiertage.jarmedia.de/api/?jahr=' . $year . '&nur_daten');
+        $holidays   = file_get_contents('https://ipty.de/feiertag/api.php?do=getFeiertage&loc=SN,BY,BW,BB&outformat=Y-m-d');
         $excludes   = [];
         $timeframes = [];
         $dates      = [];
