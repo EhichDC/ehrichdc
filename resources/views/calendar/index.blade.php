39686be93fbe163db2e83fde7e4b8330a1d277ab
Julian Bertsch <julian.bertsch42@gmail.com>
2019-02-06 12:54:36 +0100

    interpretate in different way for frontend with new holiday api

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -66,9 +66,9 @@
                         @endforeach
                         @foreach(json_decode($holidays) as $k => $v)
                     {
-                        title: "{!! $k !!}",
+                        title: "{!! $v['title'] !!}",
                         allDay: true,
-                        start: '{{ $v }}',
+                        start: '{{ $v['date'] }}',
                         color: 'red'
                     },
                         @endforeach
