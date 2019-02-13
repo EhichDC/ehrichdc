a15f69bb4cc4b737dfc17d26ac906a13a0dc72fe
Julian Bertsch <julian.bertsch42@gmail.com>
2019-02-13 13:43:36 +0100

    remove autofocus after scroll to form-section

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -14,7 +14,6 @@ $(function() {
             easing: 'swing',
             scrollTarget: '#form-section',
             afterScroll: function () {
-                $("#form-section form :text").first().focus();
                 fbq('track', 'go-to-form');
             }
         });
