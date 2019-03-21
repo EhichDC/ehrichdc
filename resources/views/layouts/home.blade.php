a03e5033b20c443aa7c7c547fc674dcf252a5b43
julian bertsch <julian.bertsch42@gmail.com>
2019-03-21 08:59:11 +0100

    add google tagmanager

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -82,13 +82,22 @@
                 document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
             }
         </script>
-
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TR7X3RG');</script>
+        <!-- End Google Tag Manager -->
     @endif
     @yield('head')
 
 </head>
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TR7X3RG"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php
 if (!isset($_COOKIE['orig_ref'])) {
     if (isset($_SERVER['HTTP_REFERER']))
