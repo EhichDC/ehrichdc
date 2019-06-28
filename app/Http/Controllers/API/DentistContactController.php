37d52df15136867eb958d46cdd8cb158a7c68cf9
julian bertsch <julian.bertsch42@gmail.com>
2019-06-28 10:07:50 +0200

    fix dl

--- app/Http/Controllers/API/DentistContactController.php
+++ app/Http/Controllers/API/DentistContactController.php
@@ -98,7 +98,7 @@ class DentistContactController extends Controller
 
         //SELECT patients.*, MIN(dates.date) FROM patients LEFT JOIN dates ON patients.id = dates.dentist_contact_id AND dates.date > NOW() GROUP BY patients.id;
         if($user->lab->first()) {
-            $results = $user->lab->first()->select([
+            $results = $user->lab->first()->dentistContact()->select([
                 'dentist_contacts.*',
                 DB::raw('MIN(dates.date) as labDate'),
                 DB::raw('MIN(employee_dates.date) as empDate'),
