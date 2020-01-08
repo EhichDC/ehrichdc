6c5dd6446e444d6498e87878b0d385f72d2536ed
julian bertsch <julian.bertsch42@gmail.com>
2020-01-08 23:36:21 +0100

    change text to call the user to get in action and phone the labor

--- resources/views/pages/mail-confirmed-is-in-queue.blade.php
+++ resources/views/pages/mail-confirmed-is-in-queue.blade.php
@@ -19,113 +19,15 @@
                         Beratungsgespräch im Dentallabor melden.
                     </p>
                 @else
-                    <h1 class="centered">Vielen Dank für Ihre Bestätigung!</h1>
+                    <h1 class="centered">Vielen Dank für Ihr Vertrauen! <br>Sie haben alles richtig gemacht!</h1>
                     <section class="your-lab">
                         <div class="row">
                             <div class="medium-8 columns">
-
-                                <p>Ihr Ansprechpartner <strong>{{ $kontaktperson }}</strong> wird sich schnellstmöglich
-                                    telefonisch bei Ihnen zur Abstimmung eines Wunsch-Termines für ein ausführliches,
-                                    kostenloses Beratungsgespräch im Dentallabor melden.
-
-
-                                    @if (session('patientConfirmed'))
-                                        <section class="besttime">
-                                            <div class="row">
-                                                <div class="medium-12 large-12 large-centered columns">
-                                                    <h5><strong>Wann können wir Sie am besten erreichen?</strong></h5>
-                                                    {{ Form::open(array('url' => '/wann', 'method' => 'POST', 'files' => true)) }}
-                                                    {{ Form::hidden('patient_id',session('patientConfirmed')->id) }}
-                                                    {{ Form::hidden('lab_id',$lab->id) }}
-
-                                                    <div class="row">
-                                                        <div class="medium-6 columns">
-                                                            {{ Form::label('from','Erreichbar werktags von:') }}
-                                                            {{ Form::select('from', array(
-                                                                '' => '-- Auswählen --',
-                                                                '08:00 Uhr' => '08:00 Uhr',
-                                                                '09:00 Uhr' => '09:00 Uhr',
-                                                                '10:00 Uhr' => '10:00 Uhr',
-                                                                '11:00 Uhr' => '11:00 Uhr',
-                                                                '12:00 Uhr' => '12:00 Uhr',
-                                                                '13:00 Uhr' => '13:00 Uhr',
-                                                                '14:00 Uhr' => '14:00 Uhr',
-                                                                '15:00 Uhr' => '15:00 Uhr',
-                                                                '16:00 Uhr' => '16:00 Uhr',
-                                                                '17:00 Uhr' => '17:00 Uhr',
-                                                                '18:00 Uhr' => '18:00 Uhr',
-                                                                '19:00 Uhr' => '19:00 Uhr',
-                                                            ), null) }}
-                                                        </div>
-                                                        <div class="medium-6 columns">
-                                                            {{ Form::label('till','bis:') }}
-                                                            {{ Form::select('till', array(
-                                                                '' => '-- Auswählen --',
-                                                                '09:00 Uhr' => '09:00 Uhr',
-                                                                '10:00 Uhr' => '10:00 Uhr',
-                                                                '11:00 Uhr' => '11:00 Uhr',
-                                                                '12:00 Uhr' => '12:00 Uhr',
-                                                                '13:00 Uhr' => '13:00 Uhr',
-                                                                '14:00 Uhr' => '14:00 Uhr',
-                                                                '15:00 Uhr' => '15:00 Uhr',
-                                                                '16:00 Uhr' => '16:00 Uhr',
-                                                                '17:00 Uhr' => '17:00 Uhr',
-                                                                '18:00 Uhr' => '18:00 Uhr',
-                                                                '19:00 Uhr' => '19:00 Uhr',
-                                                                '20:00 Uhr' => '20:00 Uhr',
-                                                            ), null) }}
-                                                        </div>
-                                                    </div>
-
-                                                    <div id="weekendTimes" style="display: none">
-                                                        <div class="row">
-                                                            <div class="medium-6 columns">
-                                                                {{ Form::label('weekend_from','von:') }}
-                                                                {{ Form::select('weekend_from', array(
-                                                                    '' => '-- Auswählen --',
-                                                                    '08:00 Uhr' => '08:00 Uhr',
-                                                                    '09:00 Uhr' => '09:00 Uhr',
-                                                                    '10:00 Uhr' => '10:00 Uhr',
-                                                                    '11:00 Uhr' => '11:00 Uhr',
-                                                                    '12:00 Uhr' => '12:00 Uhr',
-                                                                    '13:00 Uhr' => '13:00 Uhr',
-                                                                    '14:00 Uhr' => '14:00 Uhr',
-                                                                    '15:00 Uhr' => '15:00 Uhr',
-                                                                    '16:00 Uhr' => '16:00 Uhr',
-                                                                    '17:00 Uhr' => '17:00 Uhr',
-                                                                    '18:00 Uhr' => '18:00 Uhr',
-                                                                    '19:00 Uhr' => '19:00 Uhr',
-                                                                ), null, []) }}
-                                                            </div>
-                                                            <div class="medium-6 columns">
-                                                                {{ Form::label('weekend_till','bis:') }}
-                                                                {{ Form::select('weekend_till', array(
-                                                                    '' => '-- Auswählen --',
-                                                                    '09:00 Uhr' => '09:00 Uhr',
-                                                                    '10:00 Uhr' => '10:00 Uhr',
-                                                                    '11:00 Uhr' => '11:00 Uhr',
-                                                                    '12:00 Uhr' => '12:00 Uhr',
-                                                                    '13:00 Uhr' => '13:00 Uhr',
-                                                                    '14:00 Uhr' => '14:00 Uhr',
-                                                                    '15:00 Uhr' => '15:00 Uhr',
-                                                                    '16:00 Uhr' => '16:00 Uhr',
-                                                                    '17:00 Uhr' => '17:00 Uhr',
-                                                                    '18:00 Uhr' => '18:00 Uhr',
-                                                                    '19:00 Uhr' => '19:00 Uhr',
-                                                                    '20:00 Uhr' => '20:00 Uhr',
-                                                                ), null, []) }}
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-{{--                                                    @include('partials.attachments_form')--}}
-                                                    {{ Form::submit('Formular abschicken',array('class' => 'button primary', 'style' => 'margin-top:1.4em;color: white;')) }}
-                                                    {{ Form::close() }}
-                                                </div>
-                                            </div>
-                                        </section>
-                                @endif
-
+                                <h2>So geht es für Sie jetzt witer:</h2>
+                                <p>Es ist für Sie nur noch <b>ein kleiner Schritt <br/>zu Ihren schönen und neuen Zähnen:</b> Rufen Sie <b><u>jetzt</u></b> in ihrem Dentallabor an und vereinbaren Sie einen <u>kostenfreien Beratungstermin</u>
+                                    <a href="tel:{{ $labtel  }}">{{ $labtel }}</a></p>
+                                <p>Denn wenn Sie neuen Zahnersatz benötigen, gehen Sie mit Padento den besten und sichersten Weg, den es gibt.</p>
+                                <p>Ihr Ansprechpartner heißt {{ $kontaktperson }} und freut sich auf Sie!</p>
                             </div>
                             <div class="medium-4 columns">
                                 <div class="callout">
