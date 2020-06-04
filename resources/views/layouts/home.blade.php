f75df5a08f13e4f678039b7f5bedebf3abc1ee4a
julian bertsch <julian.bertsch42@gmail.com>
2020-06-04 23:04:45 +0200

    add new tagmanager script

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -83,12 +83,11 @@
             }
         </script>
         <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TR7X3RG');</script>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=AW-995423199"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-995423199'); </script>
         <!-- End Google Tag Manager -->
+        @if (Request::is('danke'))
+            <!-- Event snippet for dankeseite aufgerufen conversion page --> <script> gtag('event', 'conversion', {'send_to': 'AW-995423199/7wqjCJSoqpcBEN_n09oD'}); </script>            
+        @endif
     @endif
     @yield('head')
 <style>
@@ -125,10 +124,6 @@
 </style>
 </head>
 <body>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TR7X3RG"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 <?php
 if (!isset($_COOKIE['orig_ref'])) {
     if (isset($_SERVER['HTTP_REFERER']))
