1c20e245c7f7ef632c6ff4c619b5b4d89039535a
julian bertsch <julian.bertsch42@gmail.com>
2021-06-14 22:22:48 +0200

    feat: implement new impressum

--- resources/views/pages/impressum.blade.php
+++ resources/views/pages/impressum.blade.php
@@ -15,99 +15,45 @@
                     <header class="entry-header">
                         <div class="row">
                             <div class="small-12 columns">
-                                <h1>Impressum</h1>
+                                <h1 style="text-align: left; font-size: 2.6rem;">IMPRESSUM</h1>
                             </div>
                         </div>
                     </header>
+
                     <div class="entry-content">
-                        <h2>Angaben gemäß § 5 TMG:</h2>
-                        <p>Ehrich Dental Consulting GmbH<br>
-                            Torplatz 1<br>
-                            29223 Celle<br>
-                        <h2>Vertreten durch:</h2>
-                        <p>Geschäftsführende Gesellschafterin:<br>
-                            Marina Ehrich
-                        <h2>Kontakt</h2>
-                        <p>Telefon: +49 (0) 5141 - 97 80 976<br>
-                        <p>E-Mail: buero@padento.de
-                        <h2>Registereintrag:</h2>
-                        <p>Eintragung im Handelsregister.<br>
-                            Registergericht: Lüneburg<br>
-                            Registernummer: HRB 205 391
-                        <h2>Umsatzsteuer:</h2>
-                        <p>Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:<br>
-                            USt-ID Nr: DE294076454
-                        <h2>Aufsichtsbehörde:</h2>
-                        <p>Ordnungsamt des Landkreises Celle<br>
-                            Trift 26 B,<br>
-                            29232 Celle<br>
-                            http://www.LKCelle.de<br>
-                        <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h2>
-                        <p>Marina Ehrich<br>
-                            Torplatz 1<br>
-                            29223 Celle<br>
 
-                        <h2>Streitschlichtung</h2>
-                        <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
-                            Verbraucherschlichtungsstelle teilzunehmen.
-                        <h2>Haftung für Inhalte</h2>
-                        <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach
-                            den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter
-                            jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen
-                            oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
-                            Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den
-                            allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst
-                            ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden
-                            von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
-                        <h2>Haftung für Links</h2>
-                        <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen
-                            Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen.
-                            Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
-                            Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf
-                            mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung
-                            nicht erkennbar.
-                        <p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete
-                            Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
-                            Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-                            Urheberrecht
-                        <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem
-                            deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
-                            Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung
-                            des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den
-                            privaten, nicht kommerziellen Gebrauch gestattet.
-                        <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die
-                            Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche
-                            gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden,
-                            bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden
-                            wir derartige Inhalte umgehend entfernen.
+                            <h2 style="text-align: left">Angaben gemäß § 5 TMG</h2>
+                            <p>Ehrich Dental Consulting GmbH<br>
+                                Torplatz 1<br>
+                                29223 Celle</p>
 
-                        <h2>Verwendete Fotos</h2>
+                            <p>Handelsregister: HRB 205 391<br>
+                                Registergericht: Lüneburg</p>
 
-                        <ul>
-                            <li>© Guido Grochowski - Fotolia.com</li>
-                            <li>© pearl - Fotolia.com</li>
-                            <li>© contrastwerkstatt - Fotolia.com</li>
-                            <li>© GlobalStock - istockphoto.com</li>
-                            <li>© Robert Kneschke - Fotolia.com</li>
-                            <li>© contrastwerkstatt - Fotolia.com</li>
-                            <li>© Robert Kneschke ­ Fotolia.com</li>
-                            <li>© PhotoSG ­ Fotolia.com</li>
-                            <li>© patrickheagney ­ istockphoto.com</li>
-                            <li>© mediaphotos – istockphoto.com</li>
-                            <li>© Sergey Furtaev ­ Fotolia.com</li>
-                            <li>© inkje / photocase.de</li>
-                            <li>© Fotolia_90396846 @ Antonioguillem</li>
-                            <li>© 86169462 @ josemagon</li>
-                            <li>© 81829379 @ blende40</li>
-                            <li>© shapecharge - istockphoto.com</li>
-                            <li>© Fotolia_48830911 @ contrastwerkstatt</li>
-                            <li>© Fotolia_49693439 @ Günter Menzl</li>
-                            <li>Narkose beim Zahnarzt: 106790307 © Cherries – Fotolia.com</li>
-                            <li>Die Zahnbrücke retten: 27857992 © O.K. - Fotolia.com</li>
-                            <li>#137146451 | Urheber: Maurice Tricatelle</li>
-                        </ul>
+                            <p><strong>Vertreten durch:</strong><br>
+                                Marina Ehrich<br>
+                                Torplatz 1<br>
+                                29223 Celle</p>
 
+                            <h2 style="text-align: left">Kontakt</h2>
+                            <p>Telefon: +49 (0) 5141 – 966 99 00<br>
+                                E-Mail: kontakt@ehrich-dc.de</p>
 
+                            <h2 style="text-align: left">Umsatzsteuer-ID</h2>
+                            <p>Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:<br>
+                                DE294076454</p>
+
+                            <h2 style="text-align: left">Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h2>
+                            <p>Marina Ehrich<br>
+                                Torplatz 1<br>
+                                29223 Celle</p>
+
+                            <h2 style="text-align: left">EU-Streitschlichtung</h2>
+                            <p>Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr</a>.<br> Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+
+                            <h2 style="text-align: left">Verbraucher­streit­beilegung/Universal­schlichtungs­stelle</h2><p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+                            <h3 style="color: #2a216f">Haftung für Inhalte</h3> <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p> <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p> <h3 style="color: #2a216f">Haftung für Links</h3> <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p> <p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p> <h3 style="color: #2a216f">Urheberrecht</h3> <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p> <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
                     </div>
                 </article>
             </div>
